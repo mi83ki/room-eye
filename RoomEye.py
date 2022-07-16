8360554b89c3be2219ce66641157b30f5fc46aff
Miyazaki <mr.teruterubouz@gmail.com>
2022-07-16 13:33:50 +0900

    [RoomEye] 起き上がり検知処理を追加

--- RoomEye.py
+++ RoomEye.py
@@ -9,7 +9,12 @@ from applianceController.method1_Login.NatureRemoController import NatureRemoCon
 from CvFpsCalc import CvFpsCalc
 from LieDownDetector import LieDownDetector
 
+
 class RoomEye:
+  LIGHT_OFF = 0
+  LIGHT_ON = 1
+  LIGHT_OFF_LIEDOWN = 2
+
   def __init__(self) -> None:
     self.__humanDetector = HumanDetector()
     self.__cap = cv2.VideoCapture(self.__humanDetector.getInput())
@@ -23,7 +28,7 @@ class RoomEye:
     self.__ROOM_LIGHT_NAME = os.environ.get("ROOM_LIGHT_NAME", "Your Light Name")
     # NatureRemoに接続
     self.__remo = NatureRemoController('Remo', self.__NATURE_REMO_TOKEN)
-    self.__bIllumination = False
+    self.__bIllumination = self.LIGHT_OFF
     self.__lieDownCnt = 0
     self.__personCnt = 0
     self.__noPersonCnt = 0
@@ -38,15 +43,15 @@ class RoomEye:
     self.__remo.sendOffSignal(self.__ROOM_LIGHT_NAME)
 
   def applianceControl(self):
-    if self.__bIllumination:
-      if self.__humanDetector.isPerson() > 0 or self.__lieDownDetector.isPerson():
+    if self.__bIllumination == self.LIGHT_ON:
+      if self.__humanDetector.isPerson() > 0:
         self.__noPersonCnt = 0
         if self.__lieDownDetector.isLieDown():
           self.__lieDownCnt += 1
           if self.__lieDownCnt >= config.LIE_DOWN_CNT_THREASHOLD:
             print("Lie down !!")
             self.lightOff()
-            self.__bIllumination = False
+            self.__bIllumination = self.LIGHT_OFF_LIEDOWN
             self.__lieDownCnt = 0
           else:
             print("lieDownCnt = " + str(self.__lieDownCnt))
@@ -59,23 +64,36 @@ class RoomEye:
         if time.time() - self.__noPersonStart >= config.NO_PERSON_TIME_THREASHOLD:
           print("Light Off!!")
           self.lightOff()
-          self.__bIllumination = False
+          self.__bIllumination = self.LIGHT_OFF
           self.__noPersonCnt = 0
         else:
           print("noPersonCnt = " + str(self.__noPersonCnt) + ", noPersonTime = " + format(time.time() - self.__noPersonStart, ".2f"))
-    else:
-      if self.__humanDetector.isPerson() > 0 or self.__lieDownDetector.isPerson():
-        if not self.__lieDownDetector.isLieDown():
-          self.__personCnt += 1
-          if self.__personCnt >= config.PERSON_CNT_THREASHOLD:
-            print("Light On!!")
-            self.lightOn()
-            self.__bIllumination = True
-            self.__personCnt = 0
-          else:
-            print("personCnt = " + str(self.__personCnt))
-        else:
+
+    elif self.__bIllumination == self.LIGHT_OFF:
+      if self.__humanDetector.isPerson() > 0:
+        self.__personCnt += 1
+        if self.__personCnt >= config.PERSON_CNT_THREASHOLD:
+          print("Light On!!")
+          self.lightOn()
+          self.__bIllumination = self.LIGHT_ON
           self.__personCnt = 0
+        else:
+          print("personCnt = " + str(self.__personCnt))
+      else:
+        self.__personCnt = 0
+
+    elif self.__bIllumination == self.LIGHT_OFF_LIEDOWN:
+      if self.__lieDownDetector.isWakeUp():
+        self.__personCnt += 1
+        if self.__personCnt >= config.PERSON_CNT_THREASHOLD:
+          print("Light On!!")
+          self.lightOn()
+          self.__bIllumination = self.LIGHT_ON
+          self.__personCnt = 0
+        else:
+          print("personCnt = " + str(self.__personCnt))
+      else:
+        self.__personCnt = 0
 
   def run(self):
     while self.__cap.isOpened():
@@ -85,21 +103,26 @@ class RoomEye:
         print("Ignoring empty camera frame.")
         # If loading a video, use 'break' instead of 'continue'.
         continue
+      image = cv2.flip(image, -1)
 
       # 人検知
-      image1 = self.__humanDetector.detect(image)
-      # 寝ころび検知
-      image2 = self.__lieDownDetector.detect(image)
+      image1, personImages = self.__humanDetector.detect(image)
+      # FPS表示
+      fps_color = (0, 255, 0)
+      cv2.putText(image1, "FPS:" + str(display_fps), (10, 30),
+                  cv2.FONT_HERSHEY_SIMPLEX, 1.0, fps_color, 2, cv2.LINE_AA)
+      cv2.imshow('Inference', image1)
+
+      personImages.insert(0, image)
+      if len(personImages) > 0:
+        # 寝ころび検知
+        images2 = self.__lieDownDetector.detects(personImages)
+        for index, img in enumerate(images2):
+          cv2.imshow('MediaPipe Holistic ' + str(index), img)
+
       # 家電の操作
       self.applianceControl()
 
-      # FPS表示
-      fps_color = (0, 255, 0)
-      cv2.putText(image2, "FPS:" + str(display_fps), (10, 30),
-                  cv2.FONT_HERSHEY_SIMPLEX, 1.0, fps_color, 2, cv2.LINE_AA)
-
-      cv2.imshow('Inference', image1)
-      cv2.imshow('MediaPipe Holistic', image2)
       if cv2.waitKey(5) & 0xFF == 27:
         break
 
