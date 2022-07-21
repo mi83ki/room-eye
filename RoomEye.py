c42d74d224c88b445544b24b76f593f721004892
Miyazaki <mr.teruterubouz@gmail.com>
2022-07-21 23:25:49 +0900

    [RoomEye] 誤検知対策のためdarknetのみで人検知する

--- RoomEye.py
+++ RoomEye.py
@@ -70,7 +70,7 @@ class RoomEye:
           print("noPersonCnt = " + str(self.__noPersonCnt) + ", noPersonTime = " + format(time.time() - self.__noPersonStart, ".2f"))
 
     elif self.__bIllumination == self.LIGHT_OFF:
-      if self.__humanDetector.isPerson() > 0 or self.__lieDownDetector.isPerson():
+      if self.__humanDetector.isPerson() > 0:
         self.__personCnt += 1
         if self.__personCnt >= config.PERSON_CNT_THREASHOLD:
           print("Light On!!")
