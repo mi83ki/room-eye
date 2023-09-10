4b6488fc40dbd9ce201b1586afd6af159ca193cf
Tatsuya Miyazaki <miyazaki.tatsuya.mail@gmail.com>
2023-09-10 16:45:51 +0900

    [roomeye] Fix warnings

--- roomeye/webserver/webserver.py
+++ roomeye/webserver/webserver.py
@@ -7,8 +7,7 @@ WebAPIを提供する
 from logging import Logger
 from typing import Callable
 
-from fastapi import FastAPI, File, HTTPException, Response, UploadFile, status
-from starlette.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Response
 
 from roomeye.common import datadict, logging_manager
 from roomeye.common.singleton import Singleton
@@ -35,9 +34,6 @@ class WebServer(metaclass=Singleton):
     async def post_sensors(self, body: datadict.SensorInfo) -> Response:
         """センサ情報登録要求
         curl -i -X POST -H "accept: application/json" -H "Content-Type: application/json" -d "{\"deviceId\":\"sensor15\", \"temperature\":\"25.4\", \"humidity\":\"55.4\", \"co2\":\"553.2\", \"passing\": true}" http://127.0.0.1:8000/api/v0/sensors
-
-        Returns:
-            dict: ロボット業務開始応答
         """
         logger.info({"action": "post_sensors", "body": body})
         if body.passing is not None:
